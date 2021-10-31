f67dffbc70f6fc87c31a58fa1353f443667ba1b9
PlumPeach <93464608+PlumPeach@users.noreply.github.com>
2021-10-31 12:33:33 +0530

    Add files via upload

---
+++ retbanner.py
@@ -0,0 +1,46 @@
+#!/home/rishav/anaconda3/bin/python
+from socket import *
+
+def retBanner(ip,port):
+	setdefaulttimeout(1)
+	try:
+		sock=socket()
+		sock.connect((ip,port))
+		banner=sock.recv(1024)
+		return banner
+
+	except:
+		return
+
+
+def main():
+	ip='192.168.1.5'
+	port=22
+	banner=str(retBanner(ip,port))
+	if(banner):
+		print('[*] '+ip+': '+banner)
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+age=10
+x='pass' if age>=18 else 'fail'
+print(x)
