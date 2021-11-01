30cf3ca616b33e18e325ee4b5f582ff676ff8022
PlumPeach <93464608+PlumPeach@users.noreply.github.com>
2021-11-01 09:40:53 +0530

    Add files via upload

---
+++ refinedret.py
@@ -0,0 +1,35 @@
+from socket import *
+import optparse
+from termcolor import colored
+def retBanner(ip,port):
+	setdefaulttimeout(1)
+	try:
+		sock=socket()
+		sock.connect((ip,port))
+		banner=sock.recv(1024)
+		return banner
+
+	except:
+		return
+
+
+def main():
+	parser=optparse.OptionParser('Usage of program'+' -H <target Host>  -P <Tagrget port> ')
+	parser.add_option('-H', dest='ip',type='string',help='Specify the host')
+	parser.add_option('-P',dest='port',type='int',help='Specify target port')
+	(options,args)=parser.parse_args()
+	ip=options.ip
+	port=options.port
+	if(ip==None or port==None):
+		print(parser.usage)
+		exit(0)
+	banner=str(retBanner(ip,port))
+	if(banner):
+		print(colored('[*] '+ip+'/%d: '%port+banner,'green'))
+
+
+
+if __name__=='__main__':
+	main()
+
+
